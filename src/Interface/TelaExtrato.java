62d11c219bf9e7a551fda44bff3f2d683c484496
gulopesf <gustalfreitas@gmail.com>
2026-05-30 22:45:07 -0300

    Botão Pix funcionando na tela extrato

--- src/Interface/TelaExtrato.java
+++ src/Interface/TelaExtrato.java
@@ -197,7 +197,8 @@ public class TelaExtrato extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+       new TelaPix().setVisible(true);
+       dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
